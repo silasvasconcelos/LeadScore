76d2f97c7e56a788fd0d316fe9e3d7df3b394db4
Silas Vasconcelos <silas.vasconcelos@coopersystem.com.br>
2022-09-08 02:10:31 -0300

    feat: setup agents urls in the project

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,10 @@ from rest_framework_simplejwt.views import (
 
 from .settings_drf import schema_view
 
+urlpatterns_api_v1 = [
+    path('', include('leads.api_url')),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -14,4 +18,5 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema_json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
     re_path(r'^$', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
+    path('api/v1/', include(urlpatterns_api_v1)),
 ]
