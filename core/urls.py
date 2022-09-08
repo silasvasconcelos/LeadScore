80204aa808c4cc32227b2f9749aa40095ecaa53f
Silas Vasconcelos <silas.vasconcelos@coopersystem.com.br>
2022-09-08 02:09:42 -0300

    chore: setup reDoc and Swagger urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from .settings_drf import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema_json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema_swagger_ui'),
+    re_path(r'^$', schema_view.with_ui('redoc', cache_timeout=0), name='schema_redoc'),
 ]
