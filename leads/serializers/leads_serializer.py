12a3c8d8a1dc911a6fdbc7ec1c2518898e03f166
Silas Vasconcelos <silas.vasconcelos@coopersystem.com.br>
2022-09-09 13:06:52 -0300

    feat: add validation to lead phone

--- leads/serializers/leads_serializer.py
+++ leads/serializers/leads_serializer.py
@@ -1,10 +1,24 @@
+import re
+
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from leads import models
 
 
 class LeadsSerializer(serializers.ModelSerializer):
+    phone = serializers.CharField(max_length=20, help_text="Number of phone of the agent")
+
     class Meta:
         model = models.Lead
         fields = ('id', 'name', 'phone', 'created_at', 'updated_at')
         read_only_fields = ('id', 'created_at', 'updated_at',)
+
+    def validate_phone(self, phone):
+        phone_cleaned = re.sub(r'\W', '', phone)
+
+        if len(phone_cleaned) != 11:
+            raise serializers.ValidationError({
+                "phone": _("The phone must equal (DD) 00000-0000 or 0000000-0000, but must be 11 digits"),
+            })
+        return phone_cleaned
