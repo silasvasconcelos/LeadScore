51345deb06fcbf1e3c57d1dbe690c3cce02f0012
Silas Vasconcelos <silas.vasconcelos@coopersystem.com.br>
2022-09-09 03:28:30 -0300

    feat: plug agent manager to agent model

--- leads/models/agent.py
+++ leads/models/agent.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+from .managers import AgentManager
+
 class Agent(models.Model):
     name = models.CharField(_("Name"), max_length=200, help_text="Name of the agent")
     level = models.FloatField(_("Level"), help_text="Level of the agent")
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True, null=True, editable=False)
     updated_at = models.DateTimeField(_("Updated At"), auto_now=True, null=True, editable=False)
 
+    objects = AgentManager()
     class Meta:
         verbose_name = _("Agent")
         verbose_name_plural = _("Agents")
