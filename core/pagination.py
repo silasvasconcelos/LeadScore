01749d172eeb4e356d21cc6ad961b0e4e2e51578
Silas Vasconcelos <silas.vasconcelos@coopersystem.com.br>
2022-09-09 03:56:29 -0300

    fix: add check in have default value in DRF settings

--- core/pagination.py
+++ core/pagination.py
@@ -4,11 +4,12 @@ from rest_framework.response import Response
 
 
 class PageNumberWithLimitPagination(PageNumberPagination):
-    page_size = api_settings.PAGE_SIZE
+    page_size = api_settings.PAGE_SIZE or 15
     page_size_query_param = 'limit'
     max_page_size = 100
     total_pages = 0
     per_page = 0
+
 
     def get_paginated_response(self, data):
         return Response({
